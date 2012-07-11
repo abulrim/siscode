b55aa3d0daff39f1066bd1fbd2a473f6f6111dea
teddy <teddy@teddy-desktop>
2012-07-11 16:13:28 +0300

    Revert to old Migration and add Static Logic plugin

---
+++ app/Plugin/StaticLogic/View/Helper/HtmlLogicHelper.php
@@ -0,0 +1,156 @@
+<?php
+class HtmlLogicHelper extends AppHelper{
+
+	var $helpers=array('Html');
+        
+        protected $_settings=array();
+        
+        public $cssTags='';
+        
+        public $scriptTags='';
+        
+        
+        protected $_later='';
+        
+        function __construct(View $view, $settings = array()) {
+            parent::__construct($view, $settings);
+        }
+        
+        
+        public function css($path,$rel=null,$options=array()){
+            $fileName=$path;
+            
+            if(!preg_match("/http|min|www/",$path)){
+                $path=str_replace('.css','',$path);
+                $fullMinPath=APP . DS . 'webroot' . DS .'css' . DS . str_replace('/',DS,$path). '.min.css';
+                
+                /*
+                if(Configure::read('debug')>0){
+                     $fullPath=APP . DS . 'webroot'. DS .'css' . DS . str_replace('/',DS,$path). '.css';
+                     //YuiCompressor::compress($fullPath,$fullMinPath);
+                }
+                 */
+                if(Configure::read('debug')== 0 && file_exists($fullMinPath)){
+                    $fileName=$path.'.min';
+                }
+            }
+            if (isset($options['inline']) && !$options['inline']) {
+                    $options['inline']=true;
+                    $this->cssTags.=$this->Html->css($fileName,$rel,$options);
+                    return '';
+            } else {
+                    return $this->Html->css($fileName,$rel,$options);
+            }
+            
+        }
+        
+        public function script($path, $options=array()){
+            $fileName=$path;
+            
+             if(!preg_match("/http|min|www/",$path)){
+                $path=str_replace('.js','',$path);
+                $fullMinPath=APP . DS . 'webroot' . DS .'js' . DS . str_replace('/',DS,$path). '.min.js';
+               /*
+                if(Configure::read('debug')>0){
+                
+                     $fullPath=APP . DS . 'webroot'. DS .'js' . DS . str_replace('/',DS,$path). '.js';
+                     //YuiCompressor::compress($fullPath,$fullMinPath);
+                }
+                
+                */
+                if(Configure::read('debug')== 0 && file_exists($fullMinPath)){
+                    $fileName=$path.'.min';
+                }
+            }
+            
+            if (isset($options['inline']) && !$options['inline']) {
+                    $options['inline']=true;
+                    $this->scriptTags.=$this->Html->script($fileName,$options);
+                    return '';
+            } else {
+                    return $this->Html->script($fileName,$options);
+            }
+            
+        }
+        
+        public function later($string=null) {
+                if ($string === null) {
+                        return $this->_later;
+                } else {
+                        $this->_later.=$string;
+                }
+        }
+        
+        /*
+        public function cssOld($path, $rel = null, $options = array()){
+            $path=str_replace('.css','',$path);
+            $fullPath=APP . DS . 'webroot'. DS .'css' . DS . str_replace('/',DS,$path). '.css';
+            $fullMinPath=APP . DS . 'webroot'. DS .'css' .DS. 'min' . DS . str_replace('/',DS,$path). '.min.css';
+            
+            $minDirectory=APP . DS . 'webroot'. DS .'css' .DS. 'min';
+            
+            $fileName=$path;
+           
+            if(!preg_match("/http|min/",$path)){
+                if(Configure::read('debug')>0 && !file_exists($fullMinPath)){
+                    
+                    $pathArray=explode('/',$path);
+                    foreach($pathArray as $i=>$dir){
+                        $minDir=$minDirectory . DS . $dir;
+                        if($i==(count($pathArray)-1)){
+                            break;
+                        }
+                        if(!is_dir($minDir)){
+                            mkdir($minDir);
+                        }
+                    }
+
+                     YuiCompressor::compress($fullPath,$fullMinPath);
+                }
+                
+                if(file_exists($fullMinPath)){
+                    $fileName='min/'.$path.'.min';
+                }
+            }
+
+            return $this->Html->css($fileName,$rel,$options);
+        }
+        
+        public function scriptOld($path, $options=array()){
+            
+            $path=str_replace('.js','',$path);
+            $fullPath=APP . DS . 'webroot'. DS .'js' . DS . str_replace('/',DS,$path). '.js';
+            $fullMinPath=APP . DS . 'webroot'. DS .'js' .DS. 'min' . DS . str_replace('/',DS,$path). '.min.js';
+            
+            $minDirectory=APP . DS . 'webroot'. DS .'js' .DS. 'min';
+            
+            $fileName=$path;
+           
+            if(!preg_match("/http|min/",$path)){
+                if(Configure::read('debug')>0 && !file_exists($fullMinPath)){
+                    
+                    $pathArray=explode('/',$path);
+                    foreach($pathArray as $i=>$dir){
+                        $minDir=$minDirectory . DS . $dir;
+                        if($i==(count($pathArray)-1)){
+                            break;
+                        }
+                        if(!is_dir($minDir)){
+                            mkdir($minDir);
+                        }
+                    }
+
+                     YuiCompressor::compress($fullPath,$fullMinPath);
+                }
+                
+                if(file_exists($fullMinPath)){
+                    $fileName='min/'.$path.'.min';
+                }
+            }
+            
+           
+            
+            return $this->Html->script($fileName,$options);
+        }
+        */
+}
